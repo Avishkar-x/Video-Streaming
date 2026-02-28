1e7ec3292461d96cb5db97f3268e843b492a8175
Avishkar Mali <avishkarmali01@gmail.com>
2026-02-28 16:31:52 +0530

    endpoints errors

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -208,7 +208,11 @@ const changePassword = asyncHandler(async(req,res)=>{
 const getCurrentUser = asyncHandler(async(req,res) =>{
     return res 
     .status(200)
-    .json(200,req.user,"current user fetched successfully")
+    .json(new apiResponse(
+        200,
+        req.user,
+        "current user fetched successfully")
+    )
 })
 const updateAccountDetails = asyncHandler(async(req, res) => {
     const {fullName, email} = req.body
